b8ebb1f713aec15eb342c326f27811c5034b2843
rishabhrpg <r.gausai@gmail.com>
2020-11-26 16:30:54 +0530

    add. retry all button to retry all msg at once

---
+++ js/extend.js
@@ -0,0 +1,35 @@
+if (findActiveMq()) {
+  console.info("Retry All Dead Letter Queue (Active MQ)");
+  const loaderUrl = chrome.extension.getURL('loader.gif');
+  if (findDLQHeading()) {
+    findDLQHeading().innerHTML += "<a href='#' id='active_mq_retry_all_button' style='float:right'>Retry All</a>";
+    findDLQHeading().innerHTML += "<img id='active_mq_retry_loader' style='float:right;display: none; width: 20px'>Retry All</img>";
+    document.getElementById('active_mq_retry_loader').src = loaderUrl;
+    bindRetryAll();
+  }
+}
+
+function findActiveMq() {
+  return document.getElementById('activemq_logo');
+}
+
+function findDLQHeading() {
+  return [...document.querySelectorAll('h2')].filter(h2 => h2.innerText.match('Browse DLQ'))[0];
+}
+
+function retryAll() {
+  document.getElementById('active_mq_retry_all_button').style.display = 'none';
+  document.getElementById('active_mq_retry_loader').style.display = 'block';
+  const allPromises = [];
+  [...document.querySelectorAll('a')].filter(a => a.innerText === 'Retry').forEach(anchor => {
+    allPromises.push(fetch(anchor.href));
+  });
+  Promise.any(allPromises).then((meta) => {
+    console.log('Done retry');
+    location.reload();
+  });
+}
+
+function bindRetryAll() {
+  document.getElementById('active_mq_retry_all_button').addEventListener('click', retryAll);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Retry All Dead Letter Queue (Active MQ)
+
+Gives you a button to retry all Dead Letter Queue Jobs
